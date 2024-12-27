2741ac5c80a6bf07ddc5ccdfc9d81877159e8533
ERAF-2407 <efren.arnaude@gmail.com>
2024-12-27 00:01:27 -0400

    feat: change name

---
+++ 01-tipos-basicos/01-variables.js
@@ -0,0 +1,10 @@
+
+let nombre = 'Hola Mundo';
+let NombreCompleto = 'UppeCamelCase'
+let nombreCompleto = 'camelCase'
+let nombre_completo = 'snake_case'
+console.log(nombre_completo);
+
+let apellido;
+let animal;
+apellido = 'Arnadue'
---
+++ 01-tipos-basicos/03-constantes.js
@@ -0,0 +1,9 @@
+
+let nombre1 = "Hola Diciembre";
+nombre1 = "Charlotte Feliz";
+console.log(nombre1);
+
+const nombre2 = "Hola Diciembre";
+//nombre2 = "Charlotte Feliz";
+console.log(nombre2);
+ 
