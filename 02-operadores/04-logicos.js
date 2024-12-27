00ae6bd8735822afcf3e2a7d84e3bbaa5397938d
ERAF-2407 <efren.arnaude@gmail.com>
2024-12-26 23:58:35 -0400

    feat: 4to commit

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,15 @@
+// AND, OR, NOT
+let mayor = false;
+let suscrito = true;
+
+// AND &&
+//console.log(true && true);
+//console.log(false && true);
+console.log('operador and', mayor && suscrito);
+
+// OR ||
+console.log('operador OR', mayor || suscrito);
+
+// NOT !
+console.log('operador not', !mayor);
+let soloCatalogoInfantil = !mayor;
